166e1ef860de731ae9bcff7c8262dc97bb081986
Bismark Kadogbe <67119909+Nero-Gh@users.noreply.github.com>
2023-11-21 10:14:03 +0000

    wrapped app component with the auth provider

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const Login = (props: Props) => {
-  return <button>Login</button>;
+  const { authenticated, setAuthenticated } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    //In here you will handle your rules
+
+    setAuthenticated(true);
+    navigate("/");
+  };
+
+  return <button onClick={handleLogin}>Login</button>;
 };
 
 export default Login;
